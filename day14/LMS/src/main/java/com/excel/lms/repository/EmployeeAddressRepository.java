8883b544c2c903ccc49617517137175045b88f45
kavyam18 <101625469+kavyam18@users.noreply.github.com>
2024-05-22 19:13:26 +0530

    Delete day14/LMS directory

--- day14/LMS/src/main/java/com/excel/lms/repository/EmployeeAddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.excel.lms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.excel.lms.entity.EmployeeAddressDetails;
-
-public interface EmployeeAddressRepository extends JpaRepository<EmployeeAddressDetails, Integer> {
-
-}
--- day14/LMS/src/main/java/com/excel/lms/repository/EmployeeExperienceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.excel.lms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.excel.lms.entity.EmployeeExperienceDetails;
-
-public interface EmployeeExperienceRepository extends JpaRepository<EmployeeExperienceDetails, Integer>  {
-
-}
